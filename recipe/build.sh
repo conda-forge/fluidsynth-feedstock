75f8f3709995d2dee0e414cdd18a14e5ec812ac6
ngam <67342040+ngam@users.noreply.github.com>
2022-01-04 18:20:47 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,5 @@
 mkdir build
 cd build
 export CC=$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-cc
-cmake .. -Denable-framework=OFF -DLIB_SUFFIX="" -Denable-libsndfile=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH:PATH=$PREFIX -CC=$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-cc
+cmake .. -Denable-framework=OFF -DLIB_SUFFIX="" -Denable-libsndfile=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH:PATH=$PREFIX
 make install
